222a0ddc4713e79eca7eb6bf232e7dd6ef56da9d
Dataanalyst-11 <mis.philmed01.dpt@gmail.com>
2026-09-16 10:52:32 +0300

    Update beibora.js

--- beibora.js
+++ beibora.js
@@ -1,7 +1,7 @@
 // Central configuration 
 // beibora.js (Beibora Configuration)
 const BEIBORA_CONFIG = {
-  API_URL: "https://api.beibora.co.ke/invoice-tracker/order-tracking/", 
+  API_URL: "https://api.beibora.co.ke/invoice-tracker/", 
   API_TOKEN: "jZYyPDJG1oZjAHhgFJPfy2fl8" // No "Bearer" prefix needed
 };
 
